0939548689315440205b52344cb2a589f479ecc7
Marchand Venter <marchand@eleo.co.za>
2021-09-14 14:50:16 +0200

    Features.

---
+++ Wheel2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wheel2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your Services page.";
+
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your Portfolio page.";
+
+            return View();
+        }
+    }
+}
---
+++ Wheel2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wheel2.Startup))]
+namespace Wheel2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
